9b2d2d0de323d0bffc6936e4281676d3dae53ab4
e1b18074 <e1b18074@st.oit.ac.jp>
2020-11-10 15:55:37 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/group7/keiji/KeijiApplication.java
@@ -0,0 +1,13 @@
+package oit.is.group7.keiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeijiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeijiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group7/keiji/KeijiApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group7.keiji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeijiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
