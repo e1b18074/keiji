fd5c7a0e8f602a75f111c8da9dc67eee7ac5ed79
e1b18074 <e1b18074@st.oit.ac.jp>
2020-11-17 16:04:51 +0900

    indexとkeijiの表示

---
+++ src/main/java/oit/is/group7/keiji/controller/StartController.java
@@ -0,0 +1,37 @@
+package oit.is.group7.keiji.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import java.security.Principal;
+
+@Controller
+public class StartController {
+
+  /**
+   * sample21というGETリクエストがあったら，sample21()を呼び出して，sample21.htmlを返すメソッド
+   *
+   * @return
+   */
+  @GetMapping("/keiji")
+  public String keiji() {
+    return "keiji.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   */
+  @GetMapping("/keiji/step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
+    return "keiji.html";
+  }
+
+}
